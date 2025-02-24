df09dae8139a35d703c5e969b431cdcbb8313c79
Jay <103248677+I-H8-YOU@users.noreply.github.com>
2025-02-24 21:08:27 +0900

    Create Heap.java

---
+++ Interface Practice/Heap.java
@@ -0,0 +1,23 @@
+package test;
+
+public class Heap implements Sort {
+
+	@Override
+	public void ascending(int[] list1) {
+		System.out.println("Heap ascending");
+		
+	}
+
+	@Override
+	public void descending(int[] list1) {
+		System.out.println("Heap descending");
+		
+	}
+
+	@Override
+	public void description() {
+		System.out.println("Heap 정렬입니다.");
+		
+	}
+
+}
